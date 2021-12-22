8fb12ce2265fd5b41fd998e191a3b8e4842d8de1
MkNice <mk1hitch@gmail.com>
2021-12-22 21:41:16 +0200

    add pages(search,comics) add components & layout & styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ComicsComponent } from './components/comics/comics.component';
+import { MarvelComponent } from './components/marvel/marvel.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MarvelComponent },
+  { path: 'comics', component: ComicsComponent },
+  { path: '**', component: NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
+import { SortComponent } from './components/sort/sort.component';
+import { HeroesListComponent } from './components/heroes-list/heroes-list.component';
+import { MarvelComponent } from './components/./marvel/marvel.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { ComicsComponent } from './components/comics/comics.component';
+import { ComicsHeaderComponent } from './components/comics-header/comics-header.component';
+import { ComicsListComponent } from './components/comics-list/comics-list.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    SortComponent,
+    HeroesListComponent,
+    MarvelComponent,
+    PaginationComponent,
+    ComicsComponent,
+    ComicsHeaderComponent,
+    ComicsListComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
