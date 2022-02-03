afa01560157e750f8a0d977f9682b35c21d95168
MkNice <mk1hitch@gmail.com>
2022-02-03 19:23:11 +0200

    improve and fix sort, becouse iject store

--- src/app/components/heroes-list/heroes-list.component.ts
+++ src/app/components/heroes-list/heroes-list.component.ts
@@ -1,9 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable, ReplaySubject } from 'rxjs';
-import { takeUntil, tap } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { IMarvelCharacters } from 'src/app/share/interfaces/interface-marvel';
-import { IDataMarvel } from 'src/app/share/interfaces/interface-data';
-import { APIService } from 'src/app/share/services/api.service';
 import { SortDataService } from './sort-data.service';
 import { Store } from '@ngrx/store';
 import { charactersErrorSelector, charactersLoadingSelector, charactersSelector, collectionSizeSelector, dataLoadCharacters } from 'src/app/reducers/marvelCharacters';
@@ -14,42 +11,23 @@ import { charactersErrorSelector, charactersLoadingSelector, charactersSelector,
   styleUrls: ['./heroes-list.component.scss']
 })
 
-export class HeroesListComponent implements OnInit, OnDestroy {
+export class HeroesListComponent implements OnInit {
 
-  public marvelHeroes: IMarvelCharacters[] = [];
-  public loading: boolean = true;
-  public selectedHero: IMarvelCharacters;
-  public collectionSize: number;
-  public numberPagesDisplay: number = 5;
-  public itemsPerPage: number = 5;
-  public limit: string = '5';
-  private destroy$: ReplaySubject<number> = new ReplaySubject<number>(1);
-
-  //Store
   public loading$: Observable<boolean> = this.store.select(charactersLoadingSelector);
   public characters$: Observable<IMarvelCharacters[]> = this.store.select(charactersSelector);
   public error$: Observable<string> = this.store.select(charactersErrorSelector);
   public collectionSize$: Observable<number> = this.store.select(collectionSizeSelector);
 
+  public numberPagesDisplay: number = 5;
+  public itemsPerPage: number = 5;
+  public limit: string = '5';
+
   constructor(
-    private apiService: APIService,
     private sortService: SortDataService,
     private store: Store) { }
 
   ngOnInit() {
     this.store.dispatch(dataLoadCharacters({}));
-    /*this.apiService.getDataCharacters()
-      .pipe(
-        tap((heroes: IDataMarvel) => {
-          this.marvelHeroes = heroes.data.results;
-          this.collectionSize = heroes.data.total;
-        }),
-        takeUntil(this.destroy$)
-      )
-      .subscribe(() => {
-        this.loading = false;
-      });
-      */
   }
 
   pagination(currentPage: number) {
@@ -61,56 +39,32 @@ export class HeroesListComponent implements OnInit, OnDestroy {
   dataFromSortLocal(param) {
     switch (param) {
       case 'By A-Z':
-        this.marvelHeroes = this.sortService.sortByAlphabetic(this.marvelHeroes);
+        this.characters$ = this.sortService.sortByAlphabetic(this.characters$);
         break;
       case 'By Z-A':
-        this.marvelHeroes = this.sortService.sortByReverseAlphabetic(this.marvelHeroes);
+        this.characters$ = this.sortService.sortByReverseAlphabetic(this.characters$);
 
         break;
       case 'By Modify':
-        this.marvelHeroes = this.sortService.sortByDate(this.marvelHeroes);
+        this.characters$ = this.sortService.sortByDate(this.characters$);
         break;
       default:
-        this.marvelHeroes = this.sortService.sortByAlphabetic(this.marvelHeroes);
+        this.characters$ = this.sortService.sortByAlphabetic(this.characters$);
     }
   }
   dataFromSortGlobal(param) {
     switch (param) {
       case 'By A-Z':
-        this.apiService.getDataCharacters('', this.limit, '', 'name')
-          .pipe(
-            tap((heroes: IDataMarvel) => this.marvelHeroes = heroes.data.results
-            ),
-            takeUntil(this.destroy$)
-          )
-          .subscribe();
+        this.store.dispatch(dataLoadCharacters({ searchName: '', limit: this.limit, offset: '', sortBy: 'name' }));
         break;
       case 'By Z-A':
-        this.apiService.getDataCharacters('', this.limit, '', '-name')
-          .pipe(
-            tap((heroes: IDataMarvel) => this.marvelHeroes = heroes.data.results),
-            takeUntil(this.destroy$)
-          )
-          .subscribe();
+        this.store.dispatch(dataLoadCharacters({ searchName: '', limit: this.limit, offset: '', sortBy: '-name' }));
         break;
       case 'By Modify':
-        this.apiService.getDataCharacters('', this.limit, '', 'modified')
-          .pipe(
-            tap((heroes: IDataMarvel) => this.marvelHeroes = heroes.data.results),
-            takeUntil(this.destroy$)
-          )
-          .subscribe();
+        this.store.dispatch(dataLoadCharacters({ searchName: '', limit: this.limit, offset: '', sortBy: 'modified' }));
         break;
       default:
-        this.apiService.getDataCharacters('', this.limit, '', 'name')
-          .pipe(
-            tap((heroes: IDataMarvel) => this.marvelHeroes = heroes.data.results),
-            takeUntil(this.destroy$)
-          )
-          .subscribe();
+        this.store.dispatch(dataLoadCharacters({ searchName: '', limit: this.limit, offset: '', sortBy: 'name' }));
     }
-  }
-  ngOnDestroy() {
-    this.destroy$.next();
   }
 }
--- src/app/components/heroes-list/sort-data.service.ts
+++ src/app/components/heroes-list/sort-data.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { IMarvelCharacters } from 'src/app/share/interfaces/interface-marvel';
 
 @Injectable({
@@ -7,16 +9,20 @@ import { IMarvelCharacters } from 'src/app/share/interfaces/interface-marvel';
 export class SortDataService {
 
   constructor() { }
-
-  public sortByAlphabetic(data: IMarvelCharacters[]): IMarvelCharacters[] {
-    return data.sort((a: IMarvelCharacters, b: IMarvelCharacters) => a.name < b.name ? -1 : 1);
+  
+    public sortByAlphabetic(data: Observable<IMarvelCharacters[]>): Observable<IMarvelCharacters[]> {
+      return data.pipe(
+        tap((arr) => arr.sort((a: IMarvelCharacters, b: IMarvelCharacters) => a.name < b.name ? -1 : 1))
+      );
+    }
+    public sortByReverseAlphabetic(data: Observable<IMarvelCharacters[]>): Observable<IMarvelCharacters[]> {
+      return data.pipe(
+        tap((arr) => arr.sort((b: IMarvelCharacters, a: IMarvelCharacters) => a.name < b.name ? -1 : 1))
+      );
+    }
+    public sortByDate(data: Observable<IMarvelCharacters[]>): Observable<IMarvelCharacters[]> {
+      return data.pipe(
+        tap((arr) => arr.sort((a: IMarvelCharacters, b: IMarvelCharacters) => a.modified < b.modified ? -1 : 1))
+      );
+    }
   }
-
-  public sortByReverseAlphabetic(data: IMarvelCharacters[]): IMarvelCharacters[] {
-    return data.sort((b: IMarvelCharacters, a: IMarvelCharacters) => a.name < b.name ? -1 : 1);
-  }
-
-  public sortByDate(data: IMarvelCharacters[]): IMarvelCharacters[] {
-    return data.sort((a: IMarvelCharacters, b: IMarvelCharacters) => a.modified < b.modified ? -1 : 1);
-  }
-}
