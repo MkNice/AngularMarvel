c43bd0ca891ba8e5aab0d088e81cd414370109b6
MkNice <mk1hitch@gmail.com>
2022-02-04 12:54:34 +0200

    add pagination in comics-list

--- src/app/components/comics-list/comics-list.component.ts
+++ src/app/components/comics-list/comics-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { charactersErrorSelector, charactersLoadingSelector, charactersSelector, dataLoadComics } from 'src/app/reducers/marvelCharacters';
+import { charactersErrorSelector, charactersLoadingSelector, charactersSelector, collectionSizeSelector, dataLoadComics } from 'src/app/reducers/marvelCharacters';
 import { IMarvelCharacters } from 'src/app/share/interfaces/interface-marvel';
 
 @Component({
@@ -14,16 +14,19 @@ export class ComicsListComponent implements OnInit {
   public loading$: Observable<boolean> = this.store.select(charactersLoadingSelector);
   public marvelComics$: Observable<IMarvelCharacters[]> = this.store.select(charactersSelector);
   public error$: Observable<string> = this.store.select(charactersErrorSelector);
-
+  public collectionSize$: Observable<number> = this.store.select(collectionSizeSelector);
+  public maxSizePages: number = 5;
+  public itemsPerPage: number = 5;
   constructor(private store: Store) { }
 
   ngOnInit(): void {
     this.store.dispatch(dataLoadComics({ params: { limit: '5' } }));
   }
-
-  public nextPage(comics) {
-    this.marvelComics$ = comics;
+  public pagination(curentPage: number) {
+    const offset = `${(this.itemsPerPage * curentPage) - this.itemsPerPage}`;
+    this.store.dispatch(dataLoadComics({ params: { limit: '5', offset: offset } }));
   }
+
   public dataFromSort(param) {
     switch (param) {
       case 'By A-Z':
